31ea486ee678e9d62525ad0a7db6725f2a197827
Kylabear <alphabangachon@gmail.com>
2024-11-23 12:33:34 +0800

    30

--- scripts/db.js
+++ scripts/db.js
@@ -2,12 +2,12 @@ module.exports = pool;
 
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root', 
-    password: 'yourpassword',
-    database: 'peak_baguio'
-});
+mysq12.connect('Mariadb://localhost:3000/my_database', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('Database connected'))
+    .catch((error) => console.error('Database connection error:', error));
 
-module.exports = pool;
+module.exports = mysq12;
 
